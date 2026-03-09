e1f333cf14dea6047a1f2d502a86666d6f7940f3
Eluã Ramos Coutinho <eluaramos@hotmail.com>
2026-03-09 11:34:24 -0300

    enviando arquivo index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$valor = 16;
+
+    if ($valor <= 10) {
+        echo "Criança";
+    } else if ($valor > 10 & $valor < 18) {
+        echo "Adolescente";
+    } else {
+        echo "Adulto";
+    }
+?>
